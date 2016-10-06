721e94895c5476cf7c1d60a13eed7bfd442cb191
fbaldauf <frederik.baldauf93@gmail.com>
2016-10-06 20:17:28 +0200

    Unit Testing hinzugefügt

---
+++ tests/jsTests.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Refactored date examples</title>
+
+<link rel="stylesheet" href="../includes/jQuery/qunit-2.0.1.css">
+<script src="../includes/jQuery/qunit-2.0.1.js"></script>
+
+<script src="../includes/jQuery/jquery-3.1.js" type="text/javascript"></script>
+<script src="../includes/jQuery/jquery.flip.min.js"
+	type="text/javascript"></script>
+
+<script src="../src/js/game.js"></script>
+
+<script src="js/gameTest.js"></script>
+</head>
+<body>
+
+	<div id="qunit"></div>
+
+	<div id="fixture"></div>
+</body>
+</html>
