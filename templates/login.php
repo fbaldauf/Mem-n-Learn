725defd857ff6ca9e8cbe6e0261ea32b009d0d9a
fbaldauf <frederik.baldauf93@gmail.com>
2016-10-23 20:20:41 +0200

    Style Login & Register

--- templates/login.php
+++ templates/login.php
@@ -1,25 +1,35 @@
-<div class="form-group">
-    
-    <?php if (isset($this->_['errmessage']) AND strlen($this->_['errmessage'])>0 ){ ?>
-        <div class="alert alert-danger"><?php echo $this->_['errmessage']; ?></div>
-    <?php } ?>
-    
-            <h1><?php echo $this->_('LOGIN_TITLE') ?></h1>
-
-	<form action="." method="POST">
-		<div class="form-group">
-			<label for="txtUser"><?php echo $this->_('LOGIN_USERNAME') ?>:</label> <input type="text"
-				class="form-control" id="txtUser" name="user">
+<div class="row">
+	<div class="col-lg-1"></div>
+	<div class="col-lg-10">
+		<div class="panel panel-default">
+            <div class="panel-heading"><h1><?php echo $this->_('LOGIN_TITLE') ?></h1></div>
+            <div class="panel-body">
+				<div class="form-group">
+		    
+				    <?php if (isset($this->_['errmessage']) AND strlen($this->_['errmessage'])>0 ){ ?>
+				        <div class="alert alert-danger"><?php echo $this->_['errmessage']; ?></div>
+				    <?php } ?>
+				
+					<form action="." method="POST">
+						<div class="form-group">
+							<label for="txtUser"><?php echo $this->_('LOGIN_USERNAME') ?>:</label> <input type="text"
+								class="form-control" id="txtUser" name="user">
+						</div>
+						<div class="form-group">
+							<label for="txtPwd"><?php echo $this->_('LOGIN_PASSWORD') ?>:</label> <input type="password"
+								class="form-control" id="txtPwd" name="password">
+						</div>
+						<button type="submit" class="btn btn-default"><?php echo $this->_('LOGIN_SUBMIT') ?></button>
+					</form>
+				
+					<a href="register"><?php echo $this->_('LOGIN_REGISTER') ?></a>
+				</div>
+			</div>
 		</div>
-		<div class="form-group">
-			<label for="txtPwd"><?php echo $this->_('LOGIN_PASSWORD') ?>:</label> <input type="password"
-				class="form-control" id="txtPwd" name="password">
-		</div>
-		<button type="submit" class="btn btn-default"><?php echo $this->_('LOGIN_SUBMIT') ?></button>
-	</form>
-
-	<a href="register"><?php echo $this->_('LOGIN_REGISTER') ?></a>
+	</div>
 </div>
+
+
 <!-- 
             <table>
                 
